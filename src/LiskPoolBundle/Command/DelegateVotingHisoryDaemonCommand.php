6d1fd5d0f7550f0448de151abc6f8e97575df033
goforli.sk <support@goforli.sk>
2017-11-18 18:35:30 +0100

    Fix in Voting History daemon, it did not run as a daemon

--- src/LiskPoolBundle/Command/DelegateVotingHisoryDaemonCommand.php
+++ src/LiskPoolBundle/Command/DelegateVotingHisoryDaemonCommand.php
@@ -35,54 +35,56 @@ class DelegateVotingHisoryDaemonCommand extends ContainerAwareCommand
         $memcached = new \Memcached();
         $memcached->addServer($container->getParameter("lisk_pool.memcached.host"), $container->getParameter("lisk_pool.memcached.port"));
 
-        $offset = 0;
-        $limit = 100; // Currently the maximum retrievable at once
-        $delegates = [];
+        while(1) {
+            $offset = 0;
+            $limit = 100; // Currently the maximum retrievable at once
+            $delegates = [];
 
-        $hasNoResults = FALSE;
-        do {
-            $data = $lisk->getDelegates($offset, $limit);
-            if($data["success"] === TRUE){
-                $offset += 100;
-                if(count($data["delegates"]) > 0) {
-                    $delegates = array_merge($delegates, $data["delegates"]);
+            $hasNoResults = FALSE;
+            do {
+                $data = $lisk->getDelegates($offset, $limit);
+                if ($data["success"] === TRUE) {
+                    $offset += 100;
+                    if (count($data["delegates"]) > 0) {
+                        $delegates = array_merge($delegates, $data["delegates"]);
+                    } else {
+                        $hasNoResults = TRUE;
+                    }
+                }
+            } while ($hasNoResults === FALSE);
+
+            foreach ($delegates as $delegate) {
+                $delegateObj = $em->getRepository("LiskPoolBundle:Delegate")->findOneByAddress($delegate["address"]);
+                $delegateInfo = $lisk->getDelegateByUsername($delegate["username"]);
+                if ($delegateInfo["success"] === TRUE) {
+                    if (!$delegateObj) {
+                        $delegateObj = new Delegate();
+                        $delegateObj->setAddress($delegate["address"]);
+                    }
+                    $delegateObj->setUsername($delegate["username"]);
+                    $delegateObj->setVotedBalance($delegateInfo["delegate"]["vote"]);
+                    $delegateObj->setRank($delegateInfo["delegate"]["rank"]);
+
+                    $em->persist($delegateObj);
+                    $em->flush();
+
+                    // Create and persist a DelegateHistory object to keep track of voting power changes
+                    $delegateHistory = new DelegateHistory();
+                    $delegateHistory->setDelegate($delegateObj);
+                    $delegateHistory->setVotedBalance($delegateInfo["delegate"]["vote"]);
+
+                    $em->persist($delegateHistory);
+                    $em->flush();
                 } else {
-                    $hasNoResults = TRUE;
+                    echo "Delegate retrieval failed for delegate: " . $delegate["username"];
+                    print_r($delegate);
                 }
             }
-        } while($hasNoResults === FALSE);
 
-        foreach($delegates as $delegate){
-            $delegateObj = $em->getRepository("LiskPoolBundle:Delegate")->findOneByAddress($delegate["address"]);
-            $delegateInfo = $lisk->getDelegateByUsername($delegate["username"]);
-            if($delegateInfo["success"] === TRUE) {
-                if (!$delegateObj) {
-                    $delegateObj = new Delegate();
-                    $delegateObj->setAddress($delegate["address"]);
-                }
-                $delegateObj->setUsername($delegate["username"]);
-                $delegateObj->setVotedBalance($delegateInfo["delegate"]["vote"]);
-                $delegateObj->setRank($delegateInfo["delegate"]["rank"]);
-
-                $em->persist($delegateObj);
-                $em->flush();
-
-                // Create and persist a DelegateHistory object to keep track of voting power changes
-                $delegateHistory = new DelegateHistory();
-                $delegateHistory->setDelegate($delegateObj);
-                $delegateHistory->setVotedBalance($delegateInfo["delegate"]["vote"]);
-
-                $em->persist($delegateHistory);
-                $em->flush();
-            } else {
-                echo "Delegate retrieval failed for delegate: " . $delegate["username"];
-                print_r($delegate);
-            }
+            // Clear the Doctrine EM to prevent memory issues
+            $em->clear();
+            $output->writeln("Processing done, sleeping for 1800 seconds...");
+            sleep(1800);
         }
-
-        // Clear the Doctrine EM to prevent memory issues
-        $em->clear();
-        $output->writeln("Processing done, sleeping for 1800 seconds...");
-        sleep(1800);
     }
 }
